59a226580865e5da76c9c4c1c5ec38a0c073b448
bogdanm13 <bmarincas@gmail.com>
2016-05-08 21:45:53 +0300

    adding example

--- linux/video_processing/ffmpeg_processing.sh
+++ linux/video_processing/ffmpeg_processing.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Usage example:
+# cut GOPR0050.MP4_30fps.mp4 ss=00:00:00 t=00:00:25 cut1.mp4
+# cut GOPR0050.MP4_30fps.mp4 ss=00:01:15 t=00:00:35 cut2.mp4
+# join cut1.mp4 cut2.mp4 result.mp4
+
 set +x
 set -e
 
