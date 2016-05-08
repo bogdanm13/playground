d8505963756de168b7846aab8a92f36630e1c799
bogdanm13 <bmarincas@gmail.com>
2016-05-08 08:10:26 +0300

    ffmpeg join script

---
+++ linux/video_processing/ffmpeg_join.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+pwd=`pwd`
+input=""
+while [ $# -gt 1 ]; do 
+	input="$input $1"
+	shift
+done 
+output=$1
+
+echo "Joining files $input into $output"
+echo "Press any key to continue..." && read
+
+tmpfile=tmpfile
+
+for f in $input; do 
+	echo file "$f" >> $tmpfile
+done
+
+cd $pwd
+echo $PWD
+ffmpeg -f concat -i $tmpfile -c copy $output
+
+rm -f ${tmpfile}
