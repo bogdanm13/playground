f98a29e038276e465283f42149c7c8799a727964
bogdanm13 <bmarincas@gmail.com>
2016-05-08 10:38:03 +0300

    library like

--- linux/video_processing/ffmpeg_join.sh
+++ linux/video_processing/ffmpeg_join.sh
@@ -1,21 +1,29 @@
 #!/bin/sh
 
-input=""
-while [ $# -gt 1 ]; do 
-	input="$input $1"
-	shift
-done 
-output=$1
+_parse_args(){
+ input=""
+ while [ $# -gt 1 ]; do 
+  input="$input $1"
+  shift
+ done 
+ output=$1
+}
 
+join(){
+ tmpfile=tmpfile
+
+ for f in $input; do 
+  echo file "$f" >> $tmpfile
+ done
+
+ ffmpeg -f concat -i $tmpfile -c copy $output
+
+ rm -f ${tmpfile}
+}
+
+_parse_args $*
 echo "Joining files $input into $output"
 echo "Press any key to continue..." && read
+join
 
-tmpfile=tmpfile
 
-for f in $input; do 
-	echo file "$f" >> $tmpfile
-done
-
-ffmpeg -f concat -i $tmpfile -c copy $output
-
-rm -f ${tmpfile}
