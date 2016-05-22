835165973cdc29b6f66ded77b60171c18b96540b
bogdanm13 <bmarincas@gmail.com>
2016-05-22 22:48:46 +0300

    normalization

--- linux/video_processing/ffmpeg_to30fps.sh
+++ linux/video_processing/ffmpeg_to30fps.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 set +x 
-set -e
 
-list=`ls $1`
-echo "will process: $list"
-for video in $list; do 
+echo "will process: $*"
+sleep 2
+for video in $*; do 
 	echo "processing $video"
-	ffmpeg -y -i ${video} -r 30 -c:v libx264 -b:v 2M -strict -2 -movflags faststart ${video}_30fps.mp4
+	sleep 2
+	video_out=`basename "$video"`
+	ffmpeg -y -i ${video} -r 30 -c:v libx264 -b:v 2M -strict -2 -movflags faststart ${video_out}_30fps.mp4 2>&1 | tee ${video_out}_to30fps.log
 done
